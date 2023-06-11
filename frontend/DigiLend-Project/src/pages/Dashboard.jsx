253ed088b048e3f591892bd4f088390ca0e8c057
albertustimothyy <albertustimothyy@gmail.com>
2023-06-11 14:45:05 +0700

    disuruh ryan push :(

--- frontend/DigiLend-Project/src/pages/Dashboard.jsx
+++ frontend/DigiLend-Project/src/pages/Dashboard.jsx
@@ -6,6 +6,8 @@ import ItemList from "./sub_pages/ItemList.jsx";
 import Return from "./sub_pages/Return.jsx";
 import Borrow from "./sub_pages/Borrow.jsx";
 import EditProfile from "./sub_pages/EditProfile.jsx";
+import Group from "./sub_pages/Group.jsx";
+import ErrorPage from "./ErrorPage.jsx";
 
 const Dashboard = () => {
   const location = useLocation();
@@ -20,6 +22,8 @@ const Dashboard = () => {
         {currentPath === "/borrow" && <Borrow />}
         {currentPath === "/return" && <Return />}
         {currentPath === "/profile" && <EditProfile />}
+        {currentPath === "/group" && <Group />}
+        {currentPath !== "/item-list" && currentPath !== "/borrow" && currentPath !== "/return" && currentPath !== "/profile" && currentPath !== "/group" && <ErrorPage />}
       </div>
     </div>
   );
